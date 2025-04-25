6672b898d0acda2ac92e0f8c56c96dc66a490e8c
amosnovell <amos.waruwu09@gmail.com>
2025-04-25 11:41:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const dataKelompok = {
     "amos": 2,
     "devin": 1,
-    "joanna": 3,
+    "toji": 3,
+    "octa": 2,
     "michael": 2,
+    "Yoan": 1,
+    "Gaby": 3,
+    "Suil": 2,
   };
   
   function tampilkanKelompok() {
     const namaInput = document.getElementById("nama");
-    const nama = namaInput.value.trim().toLowerCase(); // Ubah jadi huruf kecil semua
+    const nama = namaInput.value.trim().toLowerCase();
   
     if (nama === "") {
       alert("Tolong masukkan namamu dulu ya!");
@@ -15,10 +19,12 @@ const dataKelompok = {
     }
   
     if (dataKelompok[nama]) {
-      const tampilNama = nama.charAt(0).toUpperCase() + nama.slice(1); // Kapitalisasi awal
-      document.getElementById("hasil").innerText = `Hai ${tampilNama}, kamu kelompok ${dataKelompok[nama]}!`;
+      const tampilNama = nama.charAt(0).toUpperCase() + nama.slice(1);
+      document.getElementById("hasil").innerText = 
+        `Hai ${tampilNama}, kamu kelompok ${dataKelompok[nama]}!\n\nHAVE FUNN YAAA..\nGOD BLESS 🔥✨`;
     } else {
-      document.getElementById("hasil").innerText = `Maaf ${nama}, kamu belum terdaftar di kelompok manapun.`;
+      document.getElementById("hasil").innerText = 
+        `Maaf ${nama}, kamu belum terdaftar di kelompok manapun.`;
     }
   
     namaInput.value = "";
