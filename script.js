f629283fbbd78d08f00587f4debe1517cdbd8e6c
amosnovell <amos.waruwu09@gmail.com>
2025-04-30 23:38:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const dataKelompok = {
   "zed": 1,
   "owen": 1,
   "naomi": 1,
-  "wita": 3,
+  "deva": 1,
   "gaby": 1,
   "rachel": 1,
   "michael": 2,
@@ -17,6 +17,7 @@ const dataKelompok = {
   "gween": 2,
   "rista": 2,
   "windi": 2,
+  "selly": 2,
   "toji": 3,
   "kalvari": 3,
   "suil": 3,
@@ -24,7 +25,7 @@ const dataKelompok = {
   "lolieta": 3,
   "sunita": 3,
   "dwi": 3,
-  "deva": 1,
+  "wita": 3,
   "yoseph": 3,
 };
 
